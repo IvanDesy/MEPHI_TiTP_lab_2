9468c7a6c6ea6c218189aaa5bc8a917b49359106
Ivan <vanya.des2004@gmail.com>
2024-05-04 14:28:58 +0300

    inital commit

---
+++ src/main/java/dia_b22_902/mavenproject1/main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package dia_b22_902.mavenproject1;
+
+/**
+ *
+ * @author vanya
+ */
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
